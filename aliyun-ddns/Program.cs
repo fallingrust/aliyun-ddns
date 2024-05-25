f27e65b61d4b1c654c4e0138aa8b241d0a2aa54c
fallingrust <55735959+fallingrust@users.noreply.github.com>
2024-05-25 16:40:17 +0800

    添加项目文件。

---
+++ aliyun-ddns/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace aliyun_ddns
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                await Task.Delay(1000);
+                Console.WriteLine(DateTime.Now);
+            }
+        }
+    }
+}
